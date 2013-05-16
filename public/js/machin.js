f86c497f5ac499a7dbdab28a7b096e248bd41fa9
goldy <goldy@furry.fr>
2013-05-16 23:15:14 +0200

    ++

--- public/js/machin.js
+++ public/js/machin.js
@@ -4,8 +4,23 @@
   var priv = {};
   var i;  
   
-  machin.slide = function(){
-    
+  machin.fistslide = function(truc){
+    $('#first-step').css('left', '700px');
   }
   
+  machin.onready = function(){
+    $('#encode').click(function(){
+      machin.firstslide('encode');
+    }
+    $('#decode').click(function(){
+      machin.firstslide('decode');
+    }
+  }
+  
+  window.machin = machin;
 })
+
+
+$(document).ready(function(){
+  machin.onready();
+});
