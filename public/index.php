92cfec3086818fceb38a70998229a67df78c645c
goldy <goldy@furry.fr>
2013-05-17 18:39:13 +0200

    ++

--- public/index.php
+++ public/index.php
@@ -59,7 +59,7 @@ function index_page(){
 	</div>
 	<div class="right input">
 	  <div class="description">or your file below</div>
-	  <input type="file" id="inputsimple" name="inputsimple" />
+	  <input type="file" id="inputsimple" name="inputsimple" /><br />(300,000 bytes max) 
 	</div>
 	<div class="prev">'.htmlentities('<-- prev').'</div>
 	<div class="next">'.htmlentities('next -->').'</div>
