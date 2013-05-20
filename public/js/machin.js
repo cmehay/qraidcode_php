ae1a91b86a10ea4a61ba6376c2a9dfe3ebaeb143
goldy <goldy@furry.fr>
2013-05-20 02:19:21 +0200

    ++

--- public/js/machin.js
+++ public/js/machin.js
@@ -211,7 +211,7 @@
       priv.display_filesize(this.files);
     });
     $('.text-encode').bind("keyup change", function() {
-      priv.display_textlength(this.html());
+      priv.display_textlength($('.text-encode').html());
     });
   };
   
