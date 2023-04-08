561b835013cb0a90fdaeab42b6dbcd2d6802627d
Chirstophe Mehay <cmehay@student.42.fr>
2023-04-08 17:54:29 +0200

    Migrate to tcpdf

--- qraidcode.php
+++ qraidcode.php
@@ -1096,7 +1096,7 @@ function pdf_create($qrcodes, $sha1, $nbdata, $size, $num = false, $required = f
       }
     }
     $abs_path = realpath(dirname(__FILE__) . '/' . TMPDIR);
-    $pdf->Output(abs_path . '/' . $sha1 . '.pdf', 'F');
+    $pdf->Output($abs_path . '/' . $sha1 . '.pdf', 'F');
   }
   catch (Exception $e) {
     trigger_error('TCPDF causes exception: ' . $e->getMessage());
